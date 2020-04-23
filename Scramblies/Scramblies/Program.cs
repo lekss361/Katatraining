675d34c9ac78807693f49303c742f6ee3af66b69
lekss lekss <alexandro361@gmail.com>
2020-04-23 12:42:03 +0300

    scramblies

---
+++ Scramblies/Scramblies/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Scramblies
+      //testing(Scramblies.Scramble("rkqodlw","world"), true);
+      //  testing(Scramblies.Scramble("cedewaraaossoqqyt","codewars"),true);
+      //  testing(Scramblies.Scramble("katas","steak"),false);
+      //  testing(Scramblies.Scramble("scriptjavx","javascript"),false);
+      //  testing(Scramblies.Scramble("scriptingjava","javascript"),true);
+      //  testing(Scramblies.Scramble("scriptsjava","javascripts"),true);
+      //  testing(Scramblies.Scramble("javscripts","javascript"),false);
+      //  testing(Scramblies.Scramble("aabbcamaomsccdd","commas"),true);
+      //  testing(Scramblies.Scramble("commas","commas"),true);
+      //  testing(Scramblies.Scramble("sammoc","commas"),true);
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "acdf1gdg";
+            string str2 = "c";
+            bool x=false;
+            int n = 0;
+            String str3 = str1.ToLower();
+            for (int v = 0; v < str1.Length; v++)
+            {
+                if (!char.IsLetter(str1, v))
+                {
+                    n = 1;
+                }
+            }
+            if (n != 1)
+            {
+
+
+                if (str3 == str1 || str1.Length < str2.Length)
+                {
+                    for (int i = 0; i < str2.Length; i++)
+                    {
+                        for (int z = 0; z < str1.Length; z++)
+                        {
+                            if (Char.IsLetter(str1, z))
+                            {
+
+
+                                if (str2[i] == str1[z])
+                                {
+
+                                    x = true;
+                                    str1 = str1.Remove(z, 1);
+
+                                    break;
+                                }
+                            }
+                            x = false;
+                        }
+                        if (x == false)
+                        {
+                            break;
+                        }
+
+                    }
+                }
+            }
+           
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Sort the odd/Sort the odd/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_the_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] array = new int[] { 5, 3, 2, 8, 1, 4 };
+            string num = "";
+            int c = 0;
+            if (array.Length >= 1)
+            {
+
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % 2 != 0)
+                    {
+                        num += array[i];
+                        array[i] = 0;
+                    }
+                }
+                char[] num1 = num.ToCharArray();
+                if (array.Length >= 1)
+                {
+
+
+                    char temp;
+                    for (int i = 0; i < num1.Length - 1; i++)
+                    {
+                        for (int j = i + 1; j < num1.Length; j++)
+                        {
+                            if (num1[i] > num1[j])
+                            {
+                                temp = num1[i];
+                                num1[i] = num1[j];
+                                num1[j] = temp;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == 0)
+                    {
+                        array[i] = num1[c] - '0';
+                        c++;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+            
+            
+        }
+    }
+}
